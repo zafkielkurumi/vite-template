15ce054b3cf78c4ffb4e57ccfd4d24615ac75bd9
zafkielkurumi <563693600@qq.com>
2021-05-28 14:20:44 +0800

    Update index.ts

--- src/route/index.ts
+++ src/route/index.ts
@@ -2,6 +2,14 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HelloWorld from '@/pages/HelloWorld.vue';
 
 const Pages = import.meta.glob('../pages/**/*.vue');
+const Views: {[prop: string]: any} = {};
+for (const path in Pages) {
+  if (Pages.hasOwnProperty(path)) {
+    const i = path.lastIndexOf('/');
+    Views[path.substring(i, path.length - 4)] = Pages[path]
+  }
+}
+
 
 const routes: RouteRecordRaw[] = [{
     path: '',
